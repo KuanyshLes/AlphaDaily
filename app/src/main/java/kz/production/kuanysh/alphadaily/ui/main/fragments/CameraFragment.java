57578c9f6cfbeda91b9c9c4c827990b5ac62625b
mukhitabdinazarov <mukhitabdinazarov@gmail.com>
2018-08-12 21:20:25 +0600

    second commit

---
+++ app/src/main/java/kz/production/kuanysh/alphadaily/ui/main/fragments/CameraFragment.java
@@ -0,0 +1,21 @@
+package kz.production.kuanysh.alphadaily.ui.main.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import kz.production.kuanysh.alphadaily.ui.base.BaseActivity;
+
+public class CameraFragment extends BaseActivity{
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void setUp() {
+
+
+    }
+}
